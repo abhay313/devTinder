ef08aad88610b5fa5107963f6dc040ddbdb57e37
abhay <abhayiiitk@gmail.com>
2025-01-16 00:53:10 +0530

    Get APIs

--- src/app.js
+++ src/app.js
@@ -4,20 +4,40 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Abhay",
-    lastName: "Yadav",
-    age: "24",
-    email: "abhay@gmail.com",
-    password: "pass@123",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send("User added successfully!");
   } catch (error) {
     res.status(400).send("Error in saving user : " + error.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const user = await User.findOne({ emailId: userEmail });
+    if (!user) {
+      res.status(404).send("Something went wrong");
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("user not found");
+  }
+});
+
+// GET/feed API
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(404).send("Something went wrong");
   }
 });
 
